16ba349bfd2c3d0ca19542a463da9b42475d8e33
nielraja123 <nielraja123@gmail.com>
2023-07-08 15:15:03 +0700

    Hero Section & Layout

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbars from "./Navbars";
+import Footer from "./Footer";
+export { Hero, CustomButton, Navbars, Footer };
